f8b3d3fae5976f8aaf4d83d4881a3c8e005c8c2a
Glenn Goodman <20248102+googlejam@users.noreply.github.com>
2026-02-17 11:33:06 -0500

    Update comments and clean up config script

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Define the master config file for your R5C
+# Target the specific config for Rockchip devices
 CONF="configs/rockchip/01-nanopi"
 
-# Remove the 'Heavy' stuff that causes security/bloat concerns
-# We use 'sed' to change '=y' to 'is not set'
+# Remove the 'Heavy' stuff that usually comes pre-installed
+# This converts '=y' to '# ... is not set'
 for PKG in vssr turboacc unblockneteasemusic xlnetacc zerotier adbyby-plus; do
     sed -i "s/CONFIG_PACKAGE_luci-app-$PKG=y/# CONFIG_PACKAGE_luci-app-$PKG is not set/g" $CONF
 done
 
-# Clean up build artifacts to save space
+# Standard FriendlyWrt clean-up
 sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' $CONF
 sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' $CONF
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' $CONF
