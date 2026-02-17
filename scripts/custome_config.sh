2d3c2c4c0eb184ebe6bd435f2e122401a9d47b4a
Glenn Goodman <20248102+googlejam@users.noreply.github.com>
2026-02-17 11:20:35 -0500

    Update custome_config.sh for improved debloat process
    Refactor debloat process to use a master config file and clean up build artifacts.

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Standard debloat: removing toolchain and SDK to save space
-sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
-sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
-sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
+# Define the master config file for your R5C
+CONF="configs/rockchip/01-nanopi"
 
-# --- New Debloat Section ---
-# This disables the heavy plugins that usually come pre-installed
-TARGET_CONF="configs/rockchip/01-nanopi"
-
-for PKG in luci-app-vssr luci-app-turboacc luci-app-unblockneteasemusic luci-app-xlnetacc luci-app-zerotier luci-app-adbyby-plus; do
-    sed -i "s/CONFIG_PACKAGE_${PKG}=y/# CONFIG_PACKAGE_${PKG} is not set/g" $TARGET_CONF
+# Remove the 'Heavy' stuff that causes security/bloat concerns
+# We use 'sed' to change '=y' to 'is not set'
+for PKG in vssr turboacc unblockneteasemusic xlnetacc zerotier adbyby-plus; do
+    sed -i "s/CONFIG_PACKAGE_luci-app-$PKG=y/# CONFIG_PACKAGE_luci-app-$PKG is not set/g" $CONF
 done
+
+# Clean up build artifacts to save space
+sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' $CONF
+sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' $CONF
+sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' $CONF
